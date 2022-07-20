bf6ef74953046f4927e8c8aa86f4052442a1618b
Jmoine <408030001@qq.com>
2022-07-20 11:03:21 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2022-07-20 11:00
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
